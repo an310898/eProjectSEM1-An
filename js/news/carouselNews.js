6cd6ee80d84cdb1da3121593d594f8b05b48f621
An Nguyen <an310898@gmail.com>
2021-09-05 14:50:13 +0700

    A

---
+++ js/news/carouselNews.js
@@ -0,0 +1,23 @@
+// var carouselNews = new Request('./js/corousel.json')
+
+fetch('./js/news/json/carousel.json')
+    .then(respond =>respond.json())
+    .then(data =>{
+        const carouselData = data
+        .map(article=>{
+            return `
+                <a href="newsContent.html?id=${article.title}">
+                  <div class="item">
+                    <div class="carousel-thumbnail">
+                    <img src="${article.img}">
+                   </div>
+                   <span>${article.title}</span>
+                  </div>
+                </a>
+            `
+        }).join("")
+        // console.log(carouselData)
+        document.querySelector(".owl-carousel").innerHTML = carouselData
+    })
+
+
